45728f6de38cab3e5629023df65e3476dbe373de
asln <asln@localhost.localdomain>
2018-01-08 22:49:28 +0100

    better code

--- src/main/java/de/dielicht/budget/calc/PaymentEventStreamBuilder.java
+++ src/main/java/de/dielicht/budget/calc/PaymentEventStreamBuilder.java
@@ -5,20 +5,14 @@ import java.util.stream.Stream;
 
 import de.dielicht.budget.calc.items.PaymentItem;
 import de.dielicht.budget.model.BudgetData;
-import de.dielicht.budget.model.CategoryData;
 
 public class PaymentEventStreamBuilder
 {
-    private Stream<PaymentItem> streamFrom(final CategoryData categoryData, final LocalDate calculationDay)
-    {
-        return categoryData.generateValueDates(calculationDay)
-                        .map(date -> new PaymentItem(categoryData.getName(), date, categoryData.getBetrag()));
-    }
-
     public Stream<PaymentItem> createEvents(final BudgetData data, final LocalDate calculationDay)
     {
         return data.getCategories().stream()
-                        .filter(category -> category.isAktiv())
-                        .flatMap(category -> this.streamFrom(category, calculationDay));
+            .filter(category -> category.isAktiv())
+            .flatMap(category -> category.generateValueDates(calculationDay)
+                .map(date -> new PaymentItem(category.getName(), date, category.getBetrag())));
     }
 }
